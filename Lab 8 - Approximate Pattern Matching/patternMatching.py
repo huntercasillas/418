558514e21b50d33b8a0913edfd5b6a54333f7c3d
Hunter Casillas <huntercasillas@me.com>
2019-09-07 02:20:02 -0600

    Finished Lab 8

---
+++ Lab 8 - Approximate Pattern Matching/patternMatching.py
@@ -0,0 +1,30 @@
+# Hunter Casillas
+
+def hammingDistance(p, q):
+    
+    distance = 0
+    pList = list(p)
+    qList = list(q)
+    
+    for i in range(len(pList)):
+        if pList[i] != qList[i]:
+            distance += 1
+
+return distance
+
+def patternMatching(pattern, genome, mismatches):
+    
+    answer = []
+    
+    for i in range(len(genome) - len(pattern)):
+        if hammingDistance(genome[i:i+len(pattern)], pattern) <= mismatches:
+            answer.append(str(i))
+
+print(*answer)
+
+
+pattern = "ATTCTGGA"
+genome = "CGCCCGAATCCAGAACGCATTCCCATATTTCGGGACCACTGGCCTCCACGGTACGGACGTCAATCAAATGCCTAGCGGCTTGTGGTTTCTCCTACGCTCC"
+mismatches = 3
+
+patternMatching(pattern, genome, mismatches)
