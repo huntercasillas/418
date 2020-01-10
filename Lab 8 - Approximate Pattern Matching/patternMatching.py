e25435b67ba906739aa601d29d1302062dbdc7b2
Hunter Casillas <huntercasillas@me.com>
2020-01-10 09:18:37 -0700

    Finished Lab 8

--- Lab 8 - Approximate Pattern Matching/patternMatching.py
+++ Lab 8 - Approximate Pattern Matching/patternMatching.py
@@ -1,11 +1,11 @@
-# Hunter Casillas
+# Hunter Casillas 
 
 def hammingDistance(p, q):
-    
+
     distance = 0
     pList = list(p)
     qList = list(q)
-    
+
     for i in range(len(pList)):
         if pList[i] != qList[i]:
             distance += 1
@@ -13,9 +13,9 @@ def hammingDistance(p, q):
 return distance
 
 def patternMatching(pattern, genome, mismatches):
-    
+
     answer = []
-    
+
     for i in range(len(genome) - len(pattern)):
         if hammingDistance(genome[i:i+len(pattern)], pattern) <= mismatches:
             answer.append(str(i))
