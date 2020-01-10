103ec999bfbe475e65adff6f037dfdb6cd252bc4
Hunter Casillas <huntercasillas@me.com>
2020-01-10 09:15:52 -0700

    Finished Lab 46

---
+++ Lab 46 - Multiple Approximate Matching/multipleMatching.py
@@ -0,0 +1,154 @@
+# Hunter Casillas
+
+def suffix_array(suffix):
+    length = len(suffix)
+    order = sort_characters(suffix)
+    character_class = get_class(suffix, order)
+    last = 1
+    while last < length:
+        order = sort_suffix(suffix, last, order, character_class)
+        character_class = update_class(order, character_class, last)
+        last = 2 * last
+    return order
+
+
+def sort_characters(suffix):
+    length = len(suffix)
+    order = [0] * length
+    count = dict()
+    for i in range(length):
+        count[suffix[i]] = count.get(suffix[i], 0) + 1
+    characters = sorted(count.keys())
+    previous = characters[0]
+    for char in characters[1:]:
+        count[char] += count[previous]
+        previous = char
+    for i in range(length - 1, -1, -1):
+        current = suffix[i]
+        count[current] = count[current] - 1
+        order[count[current]] = i
+    return order
+
+
+def get_class(suffix, order):
+    length = len(suffix)
+    character_class = [0] * length
+    character_class[order[0]] = 0
+    for i in range(1, length):
+        if suffix[order[i]] != suffix[order[i - 1]]:
+            character_class[order[i]] = character_class[order[i - 1]] + 1
+        else:
+            character_class[order[i]] = character_class[order[i - 1]]
+    return character_class
+
+
+def sort_suffix(suffix, last, order, character_class):
+    length = len(suffix)
+    count = [0] * length
+    updated = [0] * length
+    for i in range(length):
+        count[character_class[i]] += 1
+    for j in range(1, length):
+        count[j] += count[j - 1]
+    for i in range(length - 1, -1, -1):
+        start = (order[i] - last + length) % length
+        current = character_class[start]
+        count[current] -= 1
+        updated[count[current]] = start
+    return updated
+
+
+def update_class(order, character_class, last):
+    n = len(order)
+    updated = [0] * n
+    updated[order[0]] = 0
+    for i in range(1, n):
+        current = order[i]
+        previous = order[i - 1]
+        middle = current + last
+        previous_middle = (previous + last) % n
+        if character_class[current] != character_class[previous] \
+                or character_class[middle] != character_class[previous_middle]:
+            updated[current] = updated[previous] + 1
+        else:
+            updated[current] = updated[previous]
+    return updated
+
+
+def burrows_wheeler(text, order):
+    length = len(text)
+    bwt = [''] * length
+    nucleotides = ['$', 'A', 'C', 'G', 'T']
+    for i in range(length):
+        bwt[i] = text[(order[i] + length - 1) % length]
+
+    count_dict = dict()
+    start_dict = dict()
+    for nucleotide in nucleotides:
+        count_dict[nucleotide] = [0] * (length + 1)
+    for i in range(length):
+        current = bwt[i]
+        for nucleotide, count in count_dict.items():
+            count_dict[nucleotide][i + 1] = count_dict[nucleotide][i]
+        count_dict[current][i + 1] += 1
+    index = 0
+    for nucleotide in sorted(nucleotides):
+        start_dict[nucleotide] = index
+        index += count_dict[nucleotide][length]
+    return bwt, start_dict, count_dict
+
+
+def approximate(first, second, d):
+    error = 0
+    for i in range(len(first)):
+        if first[i] != second[i]:
+            error += 1
+            if error > d:
+                return False
+    return True
+
+
+def multiple_matching(text, patterns, d, suffix):
+    bwt, starts, counts = burrows_wheeler(text, suffix)
+    answers = []
+    for pattern in patterns:
+        current = set()
+        length = len(pattern)
+        k = length // (d + 1)
+        seeds = [(pattern[i * k:(i + 1) * k], i * k) for i in range(d)] + [(pattern[d * k:length], d * k)]
+        for seed, offset in seeds:
+            top = 0
+            bottom = len(bwt) - 1
+            index = len(seed) - 1
+            while top <= bottom:
+                if index >= 0:
+                    symbol = seed[index]
+                    index -= 1
+                    if counts[symbol][bottom + 1] - counts[symbol][top] > 0:
+                        top = starts[symbol] + counts[symbol][top]
+                        bottom = starts[symbol] + counts[symbol][bottom + 1] - 1
+                    else:
+                        break
+                else:
+                    for i in range(top, bottom + 1):
+                        current.add(suffix[i] - offset)
+                    break
+        for number in current:
+            if approximate(text[number:number + length], pattern, d):
+                answers.append(number)
+    return answers
+
+
+def get_input(filename):
+    file = open(filename, 'r')
+    data = file.read().strip().split()
+    text = data[0] + '$'
+    patterns = data[1:-1]
+    d = int(data[-1])
+    return text, patterns, d
+
+
+text, patterns, d = get_input("rosalind_ba9o.txt")
+suffix = suffix_array(text)
+answer = multiple_matching(text, patterns, d, suffix)
+print(' '.join(map(str, sorted(answer))))
